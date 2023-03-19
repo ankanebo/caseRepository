4ffbe23eb1e9e8ac60bf405a38b6e28a7c7ce193
user3 <podnebesnovaanna@gmail.com>
2023-03-19 13:26:51 +0300

    последний комит

--- app/request.py
+++ app/request.py
@@ -4,4 +4,5 @@ from constants import TOKEN, URL
 
 def request_data() -> list:
     response = requests.get(url=URL, headers={'X-Auth-Token': TOKEN})
+    print(response)
     return response.json()['message']
